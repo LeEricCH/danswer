85ccbc90b3aae38bc3b4237d12eca30d10668d1e
pablodanswer <pablo@danswer.ai>
2024-06-07 16:04:53 -0700

    add functional scroll resizing based on chat input

--- web/src/app/chat/ChatPage.tsx
+++ web/src/app/chat/ChatPage.tsx
@@ -44,7 +44,6 @@ import {
   useFilters,
   useInitialScroll,
   useLlmOverride,
-  useResponsiveScroll,
   useResponsiveScroll2,
 } from "@/lib/hooks";
 import { computeAvailableFilters } from "@/lib/filters";
@@ -1279,8 +1278,6 @@ export function ChatPage({
                             );
                           }
                         })}
-                        z
-                        <div ref={endDivRef} className="min-h-[100px]" />1 z
                         {isStreaming &&
                           messageHistory.length > 0 &&
                           messageHistory[messageHistory.length - 1].type ===
@@ -1306,6 +1303,8 @@ export function ChatPage({
                               />
                             </div>
                           )}
+
+                        <div ref={endDivRef} className="" />
                         {livePersona &&
                           livePersona.starter_messages &&
                           livePersona.starter_messages.length > 0 &&
